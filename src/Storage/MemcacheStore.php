4306f764ee7b2f963d012bfe98fc874c02395e5d
Andrius Putna <fordnox@gmail.com>
2016-01-03 17:48:40 +0200

    Add memcache storage

---
+++ src/Storage/MemcacheStore.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Cart\Storage;
+
+class MemcacheStore implements Store
+{
+    /**
+     * @var $memcache \Memcache
+     */
+    protected $memcache;
+
+    public function __construct($host, $port)
+    {
+        $memcache_obj = new \Memcache;
+        $memcache_obj->connect($host, $port);
+        $this->memcache = $memcache_obj;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function get($cartId)
+    {
+        return $this->memcache->get($cartId);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function put($cartId, $data)
+    {
+        return $this->memcache->set($cartId, $data);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function flush($cartId)
+    {
+        return $this->memcache->delete($cartId);
+    }
+}
